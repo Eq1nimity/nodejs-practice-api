c32adf0e3815077ff7bb4a0cca2de141ff323f55
eq1nimity <eq1nimity@gmail.com>
2018-06-04 09:42:05 -0400

    setup mongodb with mLab integration

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express       = require('express');
 const MongoClient   = require('mongodb').MongoClient;
 const bodyParser    = require('body-parser');
+const db            = require('./config/db');
 
 const app           = express();
+
 const port          = 8000;
 
-require('./app/routes')(app, {}); // {} is an empty object. 
+app.use(bodyParser.json());
 
-// Setup listener
-app.listen(port, () => {
-  console.log('Listening on ' + port);
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+
+  // db = database.db("nodejs-practice-api")
+  require('./app/routes')(app, database);
+
+  app.listen(port, () => {
+    console.log('Listening on ' + port);
+  });
 });

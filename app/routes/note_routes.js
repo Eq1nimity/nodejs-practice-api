877c3f874214cabd3ffab1f736795955926c1931
eq1nimity <eq1nimity@gmail.com>
2018-06-04 12:43:08 -0400

    add read and delete routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,16 +1,45 @@
+var ObjectID = require('mongodb').ObjectID;
 
 // Create Route
 module.exports = function(app, database) {
+  // Set db
+  const db = database.db("nodejs-practice-api")
+
+  // Read
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occured with read request.'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  // Create
   app.post('/notes', (req, res) => {
-    // Create notes here.
     const note = { playerData: req.body };
-    const db = database.db("nodejs-practice-api")
-      db.collection('notes').insert(note, (err, result) => {
+    db.collection('notes').insert(note, (err, result) => {
         if (err) {
-          res.send({ 'error': 'An error has occured.' });
+          res.send({ 'error': 'An error has occured with create request.' });
         } else {
           res.send(result.ops[0]);
         }
+    });
+  });
+
+  // Delete
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occured with delete request.'});
+      } else {
+        res.send('item' + id + ' deleted!');
+      }
     });
   });
 };

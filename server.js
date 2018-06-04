6a584296d1eb0640725d367be1930239a36aee15
eq1nimity <eq1nimity@gmail.com>
2018-06-04 08:44:11 -0400

    init create route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser    = require('body-parser');
 const app           = express();
 const port          = 8000;
 
+require('./app/routes')(app, {}); // {} is an empty object. 
+
 // Setup listener
 app.listen(port, () => {
   console.log('Listening on ' + port);

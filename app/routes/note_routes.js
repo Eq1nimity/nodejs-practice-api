517dd1ad64b6dcc8b257b96a23b55f97f497df1e
eq1nimity <eq1nimity@gmail.com>
2018-06-04 11:56:07 -0400

    add create JSON object data in mongodb functionality

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,16 @@
 
 // Create Route
-module.exports = function(app, db) {
+module.exports = function(app, database) {
   app.post('/notes', (req, res) => {
     // Create notes here.
-    console.log(req.body)
-    res.send('Hello')
+    const note = { playerData: req.body };
+    const db = database.db("nodejs-practice-api")
+      db.collection('notes').insert(note, (err, result) => {
+        if (err) {
+          res.send({ 'error': 'An error has occured.' });
+        } else {
+          res.send(result.ops[0]);
+        }
+    });
   });
 };
--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const app           = express();
 
 const port          = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
 
-  // db = database.db("nodejs-practice-api")
+  const db = database.db("nodejs-practice-api")
   require('./app/routes')(app, database);
 
   app.listen(port, () => {
